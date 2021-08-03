5a1c2b17d01ec0177d77f282af74d6260253c543
connectsusma <susmasagarbhattarai@gmail.com>
2021-08-03 23:31:18 +0545

    End  of Mapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt=> opt.MapFrom(src => 
+                src.Photos.FirstOrDefault(x => x.IsMain).Url))
+
+                .ForMember(dest => dest.Age, opt=> opt.MapFrom(src => 
+                src.DateOfBirth.CalculateAge()));
+
+
+            CreateMap<Photo, PhotoDto>();
+
+           // CreateMap<MemberUpdateDto, AppUser>();
+
+            CreateMap<RegisterDto, AppUser>();
+         //   CreateMap<Message, MessageDto>()
+
+        }
+    }
+}
