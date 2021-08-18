3766425f26f109489e1bf97887d11aaf910758f9
connectsusma <susmasagarbhattarai@gmail.com>
2021-08-19 02:15:12 +0545

    done

--- API/Entities/Message.cs
+++ API/Entities/Message.cs
@@ -16,6 +16,5 @@ namespace API.Entities
         public DateTime MessageSent { get; set; } = DateTime.UtcNow;
         public bool SenderDeleted { get; set; }
         public bool RecipientDeleted { get; set; }
-        
     }
 }
